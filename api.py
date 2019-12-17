d646cea0707a9edca7e9a0b51e9824747b033f00
gitadmin <gitadmin@muenai.com>
2019-12-17 15:29:30 +0800

    update

--- api.py
+++ api.py
@@ -6,8 +6,6 @@ from helper import apple_news, eyny_movie, jenkins
 
 CHANNEL_ACCESS_TOKEN = "s19Xzrw8j71uvdzTbFRxTyWHotTOS8AV+VPNDzMGi6nI/uRRrHO5giqGDQBH7AFUsu81rAilC+anC0tZpHeo/oLc819o8I4JIX6XQniJPHSKo+5cgoJOUl7jTHxviMHWV733BXr9T2Js2YkcnPzbTgdB04t89/1O/w1cDnyilFU="
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-
-
 profile = line_bot_api.get_profile('U7ee61bf4d1721b77976b3f8ac38b3ff7')
 
 print(profile.display_name)
@@ -20,5 +18,4 @@ try:
     # line_bot_api.push_message(to, TextSendMessage(text=eyny_movie()))
     line_bot_api.push_message(to, TextSendMessage(text=jenkins()))
 except LineBotApiError as e:
-    # error handle
     raise e
--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5001)
 
 
 
