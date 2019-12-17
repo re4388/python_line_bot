ea6c1e29ca0f5e47bd356380ecace05e5f4d643a
gitadmin <gitadmin@muenai.com>
2019-12-17 11:39:00 +0800

    update

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+    return 'Hello, World!'
 
 @app.route('/test')
-  def test_page():
+def test_page():
     return 'In test page!'
